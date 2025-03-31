b8a9093ea2decab694b833aeeb3a964a566c6d2e
James Couball <jcouball@yahoo.com>
2025-03-31 13:46:24 -0700

    feat: add initial_branch option to RubyGit::Worktree.init

--- spec/lib/ruby_git/worktree_init_spec.rb
+++ spec/lib/ruby_git/worktree_init_spec.rb
@@ -3,46 +3,114 @@
 require 'tmpdir'
 
 RSpec.describe RubyGit::Worktree do
-  describe '.init(worktree_path)' do
-    subject { described_class.init(worktree_path) }
-    let(:tmpdir) { Dir.mktmpdir }
-    after { FileUtils.rm_rf(tmpdir) }
+  describe '.init' do
+    subject { described_class.init(worktree_path, initial_branch: initial_branch) }
+    let(:initial_branch) { nil }
 
-    context 'when worktree_path does not exist' do
-      let(:worktree_path) { tmpdir }
-      before { FileUtils.rmdir(tmpdir) }
-      it 'should raise a RubyGit::Error' do
-        expect { subject }.to raise_error(RubyGit::Error)
+    describe 'initializing a worktree' do
+      let(:tmpdir) { @tmpdir }
+
+      around do |example|
+        in_temp_dir do |tmpdir|
+          @tmpdir = tmpdir
+          example.run
+        end
+      end
+
+      context 'when worktree_path does not exist' do
+        let(:worktree_path) { File.join(tmpdir, 'subdir') }
+
+        it 'should raise an ArgumentError' do
+          expect { subject }.to raise_error(ArgumentError)
+        end
+      end
+
+      context 'when worktree_path exists' do
+        let(:worktree_path) { tmpdir }
+        context 'and is not a directory' do
+          before do
+            FileUtils.rmdir(worktree_path)
+            FileUtils.touch(worktree_path)
+          end
+          it 'should raise an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'and is a directory' do
+          context 'and is in a working tree' do
+            before do
+              raise RuntimeError unless system('git init', chdir: worktree_path, %i[out err] => IO::NULL)
+            end
+            it 'should return a Worktree object to the existing working tree' do
+              expect(subject).to be_kind_of(RubyGit::Worktree)
+              expect(subject).to have_attributes(path: File.realpath(worktree_path))
+            end
+          end
+
+          context 'and is not in the working tree' do
+            it 'should initialize the working tree and return a Worktree object' do
+              expect(subject).to be_kind_of(RubyGit::Worktree)
+              expect(subject).to have_attributes(path: File.realpath(worktree_path))
+            end
+          end
+        end
       end
     end
 
-    context 'when worktree_path exists' do
-      let(:worktree_path) { tmpdir }
-      context 'and is not a directory' do
-        before do
-          FileUtils.rmdir(worktree_path)
-          FileUtils.touch(worktree_path)
-        end
-        it 'should raise RubyGit::Error' do
-          expect { subject }.to raise_error(RubyGit::Error)
+    describe 'constructing the git command line' do
+      let(:worktree_path) { '/my/worktree/path' }
+      let(:result) { instance_double(RubyGit::CommandLine::Result, stdout: '') }
+
+      before do
+        allow(described_class).to(
+          receive(:new).with(worktree_path).and_return(instance_double(RubyGit::Worktree))
+        )
+      end
+
+      context 'called with no arguments' do
+        let(:expected_command) { %w[init] }
+
+        it 'should run the expected git command' do
+          expect(RubyGit::CommandLine).to(
+            receive(:run).with(*expected_command, Hash).and_return(result)
+          )
+
+          subject
         end
       end
 
-      context 'and is a directory' do
-        context 'and is in a working tree' do
-          before do
-            raise RuntimeError unless system('git init', chdir: worktree_path, %i[out err] => IO::NULL)
-          end
-          it 'should return a Worktree object to the existing working tree' do
-            expect(subject).to be_kind_of(RubyGit::Worktree)
-            expect(subject).to have_attributes(path: File.realpath(worktree_path))
+      describe 'initial_branch option' do
+        context 'when nil' do
+          let(:expected_command) { %w[init] }
+
+          it 'should run the expected git command' do
+            expect(RubyGit::CommandLine).to(
+              receive(:run).with(*expected_command, Hash).and_return(result)
+            )
+            subject
           end
         end
 
-        context 'and is not in the working tree' do
-          it 'should initialize the working tree and return a Worktree object' do
-            expect(subject).to be_kind_of(RubyGit::Worktree)
-            expect(subject).to have_attributes(path: File.realpath(worktree_path))
+        context 'when a string' do
+          let(:expected_command) { ['init', '--initial-branch', initial_branch] }
+          let(:initial_branch) { 'my-branch' }
+
+          it 'should run the expected git command' do
+            expect(RubyGit::CommandLine).to(
+              receive(:run).with(*expected_command, Hash).and_return(result)
+            )
+            subject
+          end
+        end
+
+        context 'when not a string' do
+          let(:initial_branch) { 123 }
+
+          it 'should raise an ArgumentError' do
+            expect { subject }.to(
+              raise_error(ArgumentError, %(The 'initial_branch:' option must be a String or nil but was 123))
+            )
           end
         end
       end
